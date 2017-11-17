b7d85f1a8e8d20061cfc9528d069a62e39ca2955
骏钰 王 <wangjy1@ect888.com>
2017-11-17 15:05:03 +0800

    feat: 链表反转
    链表反转，直接反转，和递归反转
    
    close # title206
    
    Change-Id: Ibc3cd102945e01e8ddab9d5aa1df2326f2fbb3e3

--- main/com/ect/leetCode/linkedList/Title206.java
+++ main/com/ect/leetCode/linkedList/Title206.java
@@ -2,38 +2,45 @@ package com.ect.leetCode.linkedList;
 
 /**
  * 反转链表
+ *
+ * 递归调用
  * Reverse a singly linked list.
  */
 public class Title206 {
 
     public ListNode reverseList(ListNode head) {
 
-        if(head==null) return null;
-
-        ListNode node = head.next;
-        ListNode pre = head;
-
-        while(node!=null){
-
-            ListNode next = node.next;
-            // 如果不是最后一个节点
-            if(next!=null){
-                node.next = pre;
-                if(pre==head){
-                    pre.next=null;
-                }
-                pre = node;
-                node = next;
-            }else{
-               node.next = pre;
-                if(pre==head){
-                    pre.next=null;
-                }
-               pre = node;
-               node = null;
-            }
-
+        if (head == null) {
+            return null;
         }
-        return pre;
+        return reverseList(head, null);
+    }
+
+    private static ListNode reverseList(ListNode listNode, ListNode head) {
+
+        ListNode next = listNode.next;
+
+        listNode.next = head;
+
+        if (next != null) {
+            head = listNode;
+            return reverseList(next, head);
+        } else {
+            return listNode;
+        }
+    }
+
+
+    // 方法二 直接进行反转
+    public ListNode reverseList2(ListNode head) {
+    /* iterative solution */
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
     }
 }
