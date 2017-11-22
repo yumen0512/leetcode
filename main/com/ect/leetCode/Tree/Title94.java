6b4633011a48b79a49b34952296906be5fbb528c
wangjunyu <wangjunyu0512@163.com>
2017-11-22 22:41:29 +0800

    feat:不用递归实现中序遍历 不用递归实现中序便历 close # title94

---
+++ main/com/ect/leetCode/Tree/Title94.java
@@ -0,0 +1,55 @@
+package com.ect.leetCode.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 思路：1递归
+ *      2 用堆栈
+ */
+public class Title94 {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList<>();
+        inOrder(root,list);
+        return list;
+    }
+
+    private static void inOrder(TreeNode root,List<Integer> list){
+        if(root==null) return;
+
+        if(root.left!=null){
+            inOrder(root.left,list);
+        }
+
+        list.add(root.val);
+
+        if(root.right!=null){
+            inOrder(root.right,list);
+        }
+    }
+
+    public List<Integer> inorderTraversal2(TreeNode root) {
+
+        List<Integer> list = new ArrayList<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+        while (root!=null||!stack.isEmpty()){
+            while (root!=null){
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+
+        }
+
+        return list;
+    }
+
+}
