e1e90e2f1081c445f19813feb97948982c50a48a
骏钰 王 <wangjy1@ect888.com>
2017-11-23 13:54:58 +0800

    feat: n皇后问题临时提交
    n皇后问题临时提交
    
    Change-Id: I720442472dd26c523f0eed633244515b229d6307

---
+++ main/com/ect/leetCode/NQueens/Title51.java
@@ -0,0 +1,17 @@
+package com.ect.leetCode.NQueens;
+
+import java.util.List;
+
+/**
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such
+ * that no two queens attack each other.
+ *
+ * Created by wangjunyu on 17-11-23.
+ */
+public class Title51 {
+
+    public List<List<String>> solveNQueens(int n) {
+
+    }
+
+}
