c798780d904dc10e84c5c0fc6a7771b1c0b68a7b
骏钰 王 <wangjy1@ect888.com>
2017-11-29 14:44:37 +0800

    feat: Remove Element
    Remove Element
    
    close # title27
    
    Change-Id: I60fa0941e9fb40c2071c25128624d6de4063e6ab

---
+++ main/com/ect/leetCode/arrary/Title27.java
@@ -0,0 +1,32 @@
+package com.ect.leetCode.arrary;
+
+/**
+ * Remove Element
+ * Given nums = [3,2,2,3], val = 3,
+ *
+ * 思路：1定义i，j两个指针，i指向的都是val,j指向都是不等于val
+ *      2j从0指到n，当nums[j]和val不同，i右移动
+ *
+ * Created by wangjunyu on 17-11-29.
+ */
+public class Title27 {
+
+    public static int removeElement(int[] nums, int val) {
+
+        int i = 0;
+
+        for (int j = 0; j < nums.length; i++) {
+            if (nums[j] != val) {
+                nums[i++] = nums[j];
+            }
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 3};
+        System.out.println(removeElement(nums, 5));
+    }
+
+}
