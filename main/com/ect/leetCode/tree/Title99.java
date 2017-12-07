1551d331921e009e95b273674b9590eaa1e5aaf4
骏钰 王 <wangjy1@ect888.com>
2017-12-07 10:41:50 +0800

    feat: 修复bts二叉树里面位置错位的两个数
    bts的中序遍历是从小到大排列的
    
    close # title99
    
    Change-Id: I7b46e625ef6d5b30e270541fdc0a981f52a79f19

--- main/com/ect/leetCode/tree/Title99.java
+++ main/com/ect/leetCode/tree/Title99.java
@@ -1,66 +1,51 @@
-//package com.ect.leetCode.Tree;
-//
-//import sun.reflect.generics.tree.Tree;
-//
-//import java.util.Stack;
-//
-///**
-// * Recover Binary Search Tree
-// *
-// * Two elements of a binary search tree (BST) are swapped by mistake.
-// *
-// * Recover the tree without changing its structure.
-// *
-// * 思路：从根节点开始，找到左右两个misTake，并且标记，交换
-// */
-//public class Title99 {
-//
-//    public void recoverTree(TreeNode root) {
-//
-//        TreeNode error1 = null;
-//        TreeNode error2 = null;
-//
-//        findErrorNode(root, error1, error2);
-//
-//        if (error1 != null && error2 != null) {
-//            int val = error1.val;
-//            error1.val = error2.val;
-//            error2.val = val;
-//        }
-//
-//    }
-//
-//    private static void findErrorNode(TreeNode treeNode, TreeNode error1, TreeNode error2) {
-//
-//        if(treeNode==null) return;
-//
-//        TreeNode head = treeNode;
-//
-//        if(treeNode)
-//
-//        Stack<TreeNode> stack = new Stack<>();
-//
-//        while(stack!=null || treeNode !=null){
-//            while(treeNode.left!=null){
-//                stack.push(treeNode);
-//                treeNode = treeNode.left;
-//            }
-//
-//
-//
-//
-//        }
-//    }
-//
-//    private static void getIt(TreeNode treeNode, TreeNode error1, TreeNode error2){
-//        if(error1==null){
-//            error1 = treeNode;
-//            return;
-//        }
-//
-//        else{
-//            error2 = treeNode;
-//        }
-//    }
-//
-//}
+package com.ect.leetCode.tree;
+
+/**
+ * Recover Binary Search Tree
+ *
+ * Two elements of a binary search tree (BST) are swapped by mistake.
+ *
+ * Recover the tree without changing its structure.
+ *
+ * 思路：bts二叉树的中序遍历是从小到大的，有问题的节点必然是出现逆序的
+ */
+public class Title99 {
+
+    private TreeNode firstNode = null;
+    private TreeNode secondNode = null;
+    private TreeNode beforeNode = new TreeNode(Integer.MIN_VALUE);
+
+
+    public void recoverTree(TreeNode root){
+
+        recoverTreeFunction(root);
+
+        int temp = firstNode.val;
+        firstNode.val = secondNode.val;
+        secondNode.val = temp;
+    }
+
+
+    public  void recoverTreeFunction(TreeNode root) {
+
+        if (root == null) return;
+
+        recoverTreeFunction(root.left);
+
+        // If first element has not been found, assign it to prevElement (refer to 6 in the example above)
+        if (firstNode == null && root.val < beforeNode.val) {
+            firstNode = beforeNode;
+        }
+
+        // If first element is found, assign the second element to the root (refer to 2 in the example above)
+        if (firstNode != null && root.val < beforeNode.val) {
+            secondNode = root;
+        }
+
+        beforeNode = root;
+
+        recoverTreeFunction(root.right);
+
+    }
+
+}
