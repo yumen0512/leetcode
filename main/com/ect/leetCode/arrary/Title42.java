1a3985c8c3ba8b8f6fda1efbf31f0162fc3b8116
骏钰 王 <wangjy1@ect888.com>
2017-12-09 17:38:50 +0800

    feat:
    Change-Id: Ieb1a216f3ba7a2c94b8ade4917305f21fc758d5e

---
+++ main/com/ect/leetCode/arrary/Title42.java
@@ -0,0 +1,16 @@
+package com.ect.leetCode.arrary;
+
+/**
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ * compute how much water it is able to trap after raining.
+ *
+ * For example, Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+ * Created by Mr Wang :) on 17-12-9.
+ */
+public class Title42 {
+
+    public int trap(int[] height) {
+
+    }
+
+}
