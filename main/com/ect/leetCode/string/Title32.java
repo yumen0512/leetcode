b7822193f8acc9f6856a91b3bdb92b6bb136129a
骏钰 王 <wangjy1@ect888.com>
2017-12-12 10:44:14 +0800

    feat: 最长的选择parentheses
    用堆栈和dp两种方式求最长有效括号长度
    
    close # title32
    
    Change-Id: Ia00b2a4fcc1d8a2568c35f95277ffc4dd09cc3b5

--- main/com/ect/leetCode/string/Title32.java
+++ main/com/ect/leetCode/string/Title32.java
@@ -12,11 +12,11 @@ import java.util.Stack;
  * Another example is ")()())", where the longest valid parentheses substring is "()()", which has
  * length = 4.
  *
- * 思路：1定义一个变量记录总数
- * 2定义两个指针指向（ 和）
- * 3每次找到，不越界的情况下，sum+1
- *
- *
+ * 思路：1用栈记录未匹配的括号位置，然后两两相减找到最大值
+ * 2用dp思想，longest(i)定义在i处结尾的最大长度
+ * （1）如果s(i)='(' longest(i)=0
+ * （2）如果s(i)=')'，如果i-1是'（ 'longest(i) = longest(i-2)+2
+ * 如果i-1是')' longest(i-long(i-1)-1)是'(' longest(i-1)+2+long(i-long(i-1)-2)
  * Created by Mr Wang :) on 17-12-11.
  */
 public class Title32 {
@@ -30,16 +30,16 @@ public class Title32 {
                 if (!st.empty()) {
                     if (s.charAt(st.peek()) == '(') st.pop();
                     else st.push(i);
-                }
-                else st.push(i);
+                } else st.push(i);
             }
         }
         if (st.empty()) longest = n;
         else {
             int a = n, b = 0;
             while (!st.empty()) {
-                b = st.peek(); st.pop();
-                longest = Math.max(longest, a-b-1);
+                b = st.peek();
+                st.pop();
+                longest = Math.max(longest, a - b - 1);
                 a = b;
             }
             longest = Math.max(longest, a);
@@ -48,8 +48,34 @@ public class Title32 {
     }
 
 
+    public static int dpSolution(String s) {
+
+        int max = 0;
+
+        int[] longest = new int[s.length()];
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    longest[i] = (i - 2) >= 0 ? longest[i - 2] + 2 : 2;
+                } else if (s.charAt(i - 1) == ')' && i - longest[i - 1] - 1 >= 0) {
+                    if (s.charAt(i - longest[i - 1] - 1) == '(') {
+                        longest[i] = longest[i - 1] + 2 + (i - longest[i - 1] - 2 >= 0 ? longest[i - longest[i - 1] - 2] : 0);
+                    }
+                }
+            }
+
+            if (longest[i] > max) {
+                max = longest[i];
+            }
+        }
+
+        return max;
+    }
+
+
     public static void main(String[] args) {
-        String str = ")))(())";
-        System.out.println(longestValidParentheses(str));
+        String str = "())";
+        System.out.println(dpSolution(str));
     }
 }
