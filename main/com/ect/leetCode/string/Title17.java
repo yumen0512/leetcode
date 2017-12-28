17a53b0f173baab6be53eba650eb6a050288734e
骏钰 王 <wangjy1@ect888.com>
2017-12-28 11:36:10 +0800

    feat: Letter Combinations of a Phone Number
    Letter Combinations of a Phone Number
    
    close #title17
    
    Change-Id: I2be071a565cc7bb060c3d577d9e42f360610226c

---
+++ main/com/ect/leetCode/string/Title17.java
@@ -0,0 +1,37 @@
+package com.ect.leetCode.string;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Letter Combinations of a Phone Number
+ *
+ * Given a digit string, return all possible letter combinations that the number could represent.
+ *
+ * A mapping of digit to letters (just like on the telephone buttons) is given below.
+ *
+ * Input:Digit string "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ *
+ * Created by Mr Wang :) on 17-12-28.
+ */
+public class Title17 {
+
+
+    public List<String> letterCombinations(String digits) {
+        LinkedList<String> ans = new LinkedList<>();
+        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ans.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            int x = Character.getNumericValue(digits.charAt(i));
+            while (ans.peek().length() == i) {
+                String t = ans.remove();
+                for (char s : mapping[x].toCharArray())
+                    ans.add(t + s);
+            }
+        }
+        return ans;
+    }
+
+
+}
