24326da84aafc1a81dfb29d8161f3e1e9e306c76
骏钰 王 <wangjy1@ect888.com>
2018-01-31 14:43:06 +0800

    feat: n皇后问题
    n皇后问题
    
    Change-Id: I31270ab474297c057ed688bbd44009603a17b001

--- main/com/ect/leetCode/nQueens/Title51.java
+++ main/com/ect/leetCode/nQueens/Title51.java
@@ -1,17 +1,89 @@
-//package com.ect.leetCode.NQueens;
-//
-//import java.util.List;
-//
-///**
-// * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such
-// * that no two queens attack each other.
-// *
-// * Created by wangjunyu on 17-11-23.
-// */
-//public class Title51 {
-//
-//    public List<List<String>> solveNQueens(int n) {
-//
-//    }
-//
-//}
+package com.ect.leetCode.nQueens;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such
+ * that no two queens attack each other.
+ *
+ * 思路：递归回溯
+ *
+ * 判断两个皇后不在一条斜线
+ * |y1-y2| ==|x1-x2|
+ *
+ * Created by wangjunyu on 17-11-23.
+ */
+public class Title51 {
+
+    public static List<List<String>> solveNQueens(int n) {
+
+        List<List<String>> lists = new ArrayList<>();
+
+        char[][] nums = new char[n][n];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                nums[i][j] = '.';
+            }
+        }
+
+        dfsSearch(lists, 0, nums);
+
+        return lists;
+
+    }
+
+
+    private static void dfsSearch(List<List<String>> lists, int row, char[][] nums) {
+
+        if (row == nums.length) {
+            buildResult(lists, nums);
+            return;
+        }
+
+
+        for (int j = 0; j < nums[0].length; j++) {
+            if (validate(nums, row, j)) {
+                nums[row][j] = 'Q';
+                dfsSearch(lists, row + 1, nums);
+                nums[row][j] = '.';
+            }
+        }
+
+
+    }
+
+
+    private static boolean validate(char nums[][], int row, int column) {
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                if (nums[i][j] == 'Q') {
+                    if (column == j || Math.abs(j - column) == Math.abs(i - row)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static void buildResult(List<List<String>> lists, char[][] nums) {
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            String str = new String(nums[i]);
+            list.add(str);
+        }
+
+        lists.add(list);
+    }
+
+    public static void main(String[] args) {
+        solveNQueens(4);
+    }
+
+
+}
